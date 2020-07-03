bffdd474aa8dfa241b936c10bca8ce9bedee1904
Hsinheng Hsu <hsin@Hsinhengs-MBP.fios-router.home>
2020-07-03 15:03:53 -0400

    work with excel

---
+++ some_result.py
@@ -0,0 +1,5 @@
+all_data = {'AK': {'Aleutians East': {'pop': 3141, 'tracts': 1},
+        'Aleutians West': {'pop': 5561, 'tracts': 2},
+        'Anchorage': {'pop': 291826, 'tracts': 55},
+        'Bethel': {'pop': 17013, 'tracts': 3},
+        'Bristol Bay': {'pop': 997, 'tracts': 1}}}
